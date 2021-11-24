9d0b15d536f1f25f8a4fc0c2910afdf7491a77d9
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-11-24 20:30:12 +0100

    longest word

---
+++ script4.js
@@ -0,0 +1,21 @@
+function findLongestWordLength(str) {
+    let words= str.split(" ");
+    let longest = 0;
+    for(let i =0; i<words.length; i++){
+     if(words[i].length > longest){
+       longest = words[i].length;
+        }
+  
+      }
+    return longest;
+    
+    }
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog"));
+  
+  findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
+  //usando map.
+
+//   function findLongestWordLength(str) {
+//     return Math.max(...str.split(" ").map(word => word.length));
+//   }
