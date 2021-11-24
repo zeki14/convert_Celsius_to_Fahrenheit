9c73c2a63c5d3dd6aec44937c026a9f592a60988
Ezequiel Gonzalez <zequi14@hotmail.com>
2021-11-24 21:39:53 +0100

    String ending

---
+++ script6.js
@@ -0,0 +1,7 @@
+//Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+    return str.slice(-target.length) === target;
+   }
+   
+   confirmEnding("Bastian", "n");
